558aeb1dd9524116a1d660877020d087bb30ae2b
Sahil <2251sahil@gmail.com>
2024-03-12 23:50:57 +0530

    update resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,41 +1,51 @@
-import React from 'react'
-import './AboutMe.css'
+import React from "react";
+import "./AboutMe.css";
 import { HiOutlineDownload } from "react-icons/hi";
-import { MdOpenInNew } from "react-icons/md"
-import { FaArrowRightLong } from "react-icons/fa6"
-import Resume from "../../public/assets/pdf/sahil_somwanshi_resume.pdf"
+import { MdOpenInNew } from "react-icons/md";
+import { FaArrowRightLong } from "react-icons/fa6";
+import Resume from "../../public/assets/pdf/sahil_resume.pdf";
 
 function AboutMe() {
   return (
-    <div id='about' className='about'>
+    <div id="about" className="about">
       <h1>About Me</h1>
       <p className="about--info">
-        <li>🎓 I'm a final year student pursuing Bachelors of Technology in Electronics and Telecommunication Engineering</li>
+        <li>
+          🎓 I'm a final year student pursuing Bachelors of Technology in
+          Electronics and Telecommunication Engineering
+        </li>
         <li>🚀 Passionate about building interactive web applications.</li>
-        <li>📚 Continuous learner with a keen interest in staying updated with the latest tech trends.</li>
-        <li>🌈 Enjoy creating intuitive and visually appealing user experiences.</li>
+        <li>
+          📚 Continuous learner with a keen interest in staying updated with the
+          latest tech trends.
+        </li>
+        <li>
+          🌈 Enjoy creating intuitive and visually appealing user experiences.
+        </li>
         <li>🎮 Outside coding, I love exploring new places, reading books.</li>
       </p>
 
       <div className="about--button">
-        <button className='about--button--resume'>
+        <button className="about--button--resume">
           Resume
-          <FaArrowRightLong size={25} style={{ marginLeft: '10px' }} />
-          <a href="https://drive.google.com/file/d/107ZjkEADvNjnbqYvwO8ufJjaa71OqBjP/view?usp=sharing" target="_blank">
-            <button className='about--button--open'>
+          <FaArrowRightLong size={25} style={{ marginLeft: "10px" }} />
+          <a
+            href="https://drive.google.com/file/d/1uWkR3e3i0h6QfFLo268n-3J9-_x-E30G/view?usp=sharing"
+            target="_blank"
+          >
+            <button className="about--button--open">
               <MdOpenInNew size={30} />
             </button>
           </a>
-          <a href={Resume} download="sahil_somwanshi_resume.pdf">
-            <button className='about--button--download'>
+          <a href={Resume} download="sahil_resume.pdf">
+            <button className="about--button--download">
               <HiOutlineDownload size={30} />
             </button>
           </a>
         </button>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
