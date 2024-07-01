b7e26de898edf4036808e205c5180b11fdef70ff
Sahil <2251sahil@gmail.com>
2024-07-01 20:15:45 +0530

    added new project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,44 +1,49 @@
-import React from 'react'
-import './Project.css'
-import { projects } from './data';
-import { TbWorld } from 'react-icons/tb'
-import { TbBrandGithubFilled } from 'react-icons/tb'
+import React from "react";
+import "./Project.css";
+import { projects } from "./data";
+// import { TbWorld } from "react-icons/tb";
+import { TbBrandGithubFilled } from "react-icons/tb";
 
 function Project() {
   return (
-    <div id='projects' className='project'>
-        <h1>Projects</h1>
-        <div className='project--card'>
+    <>
+      <h1 className="heading">Projects</h1>
+      <div id="projects" className="project">
+        <div></div>
+        <div className="project--card--container">
           {projects.map((project) => (
-              <div key={project.image} className='project--item'>
-                <div className="project--image--container">
-                  <img
-                    alt="gallery"
-                    src={project.image}
-                    className='project--img'
-                  />
-                </div>
-                <div className='project--info'>
-                  <h2>
-                    {project.title}
-                  </h2>
+            <div key={project.image} className="cards">
+              <div className="project--image--container">
+                <img
+                  alt="gallery"
+                  src={project.image}
+                  className="project--img"
+                />
+              </div>
+              <div className="project--info">
+                  <h3 className="title">{project.title}</h3>
                   <p>{project.description}</p>
-                  <div className="project--info--button">
-                    <button onClick={() => window.open(project.github, '_blank')}>
-                      <TbBrandGithubFilled size={25} className='project--button--margin'/>
-                      Source Code
-                    </button>
-                    <button onClick={() => window.open(project.demo, '_blank')}>
-                      <TbWorld size={25} className='project--button--margin'/>
-                      Live Demo
-                    </button>
-                  </div>
+                <div className="project--info--button">
+                  <button onClick={() => window.open(project.github, "_blank")}>
+                    <TbBrandGithubFilled
+                      size={25}
+                      className="project--button--margin"
+                    />
+                    Source Code
+                  </button>
+                  {/* <button onClick={() => window.open(project.demo, "_blank")}>
+                    <TbWorld size={25} className="project--button--margin" />
+                    Live Demo
+                  </button> */}
                 </div>
               </div>
+            </div>
           ))}
         </div>
-    </div>
+        <div></div>
+      </div>
+    </>
   );
 }
 
-export default Project
+export default Project;
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -2,16 +2,24 @@ import Portfolio from "../../public/assets/images/Portfolio.png";
 import TodoApp from "../../public/assets/images/todoApp.png";
 import MemeVerse from "../../public/assets/images/memeVerse.png";
 import currencyconverter from "../../public/assets/images/currencyConverter.png";
-import blogVerse from "../../public/assets/images/blogVerse.png"
-
+import blogVerse from "../../public/assets/images/blogVerse.png";
+import moduleName from "../../public/assets/images/ecommerce.png";
 export const projects = [
+  {
+    title: "Ecommerce",
+    description:
+      "Welcome to the eCommerce Platform Project, built with the MERN stack (Redux, TypeScript, Sass). This project offers a robust and feature-rich online shopping experience designed to empower businesses.",
+    image: moduleName,
+    github: "https://github.com/Sahilsom08/Ecommerce_Frontend",
+    // demo: "https://blog-verse-liard.vercel.app/",
+  },
   {
     title: "BlogVerse",
     description:
       "A blog website, crafted with React and Tailwind CSS for a capitvating reading experience. Enhanced by a robust rich text editor and supported by Appwrite backend, it ensures smooth article management and secure user authentication",
     image: blogVerse,
     github: "https://github.com/Sahilsom08/BlogVerse",
-    demo: "https://blog-verse-liard.vercel.app/",
+    // demo: "https://blog-verse-liard.vercel.app/",
   },
   {
     title: "My-Portfolio",
@@ -19,7 +27,7 @@ export const projects = [
       "Discover my portfolio website, built with React.js and some libraries. Experience a showcase of my projects and skills in a user-friendly and engaging format.",
     image: Portfolio,
     github: "https://github.com/Sahilsom08/myPortfolio",
-    demo: "https://sahils-portfolioweb.netlify.app/",
+    // demo: "https://sahils-portfolioweb.netlify.app/",
   },
   {
     title: "MemeVerse",
@@ -27,7 +35,7 @@ export const projects = [
       "An interactive meme generator that seamlessly combines image customization with creativity. With the ability to overlay text on random images sourced from an API, this project exemplifies my ability to merge coding skills with imaginative design.",
     image: MemeVerse,
     github: "https://github.com/Sahilsom08/MemeVerse",
-    demo: "https://sahilsom08.github.io/MemeVerse/",
+    // demo: "https://sahilsom08.github.io/MemeVerse/",
   },
   {
     title: "Currency Converter",
@@ -35,7 +43,7 @@ export const projects = [
       "The Dynamic Currency Converter is an engaging web application that seamlessly integrates currency conversion with user interaction. By leveraging real-time exchange rates and intuitive design",
     image: currencyconverter,
     github: "https://github.com/Sahilsom08/currencyConverter",
-    demo: "https://currency-converter-one.netlify.app/",
+    // demo: "https://currency-converter-one.netlify.app/",
   },
   ,
   {
@@ -44,6 +52,6 @@ export const projects = [
       "A comprehensive todo list application with create, edit, and delete functionalities. Seamlessly managing tasks, this project demonstrates my proficiency in designing efficient and user-friendly task management solutions.",
     image: TodoApp,
     github: "https://github.com/Sahilsom08/todoApp",
-    demo: "https://bespoke-taiyaki-c96eff.netlify.app/",
+    // demo: "https://bespoke-taiyaki-c96eff.netlify.app/",
   },
 ];
